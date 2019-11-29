0d3db41c595a2157610fc63b06067e72cdf628ed
Lộc Trần Phước <giongnhuntlf@gmail.com>
2019-11-29 18:59:35 +0700

    FEATURE: Rabin-Karp algorithm

--- StringMatchingProj/StringMatchingProj/SM_Algorithm.h
+++ StringMatchingProj/StringMatchingProj/SM_Algorithm.h
@@ -1,13 +1,14 @@
 #pragma once
 #include "pch.h"
+#define d 26 //Number of charcters
 
 class SM_Algorithm
 {
 public:
-	static vector<int> Brute_Force(char* patterm, int m, char* text, int n);
-	static vector<int> Kabin_Karp(char* patterm, int m, char* text, int n);
-	static vector<int> Knuth_Morris_Pratt(char* patterm, int m, char* text, int n);
+	static vector<int> Brute_Force(char* pattern, int m, char* text, int n);
+	static vector<int> Kabin_Karp(char* pattern, char* text);
+	static vector<int> Knuth_Morris_Pratt(char* pattern, char* text);
 public:
-	static int String_Hashing(char* patt, int m);
+	static int String_Hashing(char* patt, int q);
+	static int String_ReHashing(int hash_value, char first_char, char new_char, int h, int q);
 };
-
