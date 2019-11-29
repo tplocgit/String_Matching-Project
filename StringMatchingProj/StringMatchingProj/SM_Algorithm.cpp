7e3ec4e85b5eca1c453f722991d450cd582a5b79
Lộc Trần Phước <giongnhuntlf@gmail.com>
2019-11-29 14:47:50 +0700

    CHANGE: Move POS, RESULT_STATUS from SM_Algorithm to Board

--- StringMatchingProj/StringMatchingProj/SM_Algorithm.cpp
+++ StringMatchingProj/StringMatchingProj/SM_Algorithm.cpp
@@ -1,1 +1,13 @@
 #include "SM_Algorithm.h"
+
+vector<int> SM_Algorithm::Brute_Force(char* patterm, int m, char* text, int n) {
+
+}
+
+vector<int> SM_Algorithm::Kabin_Karp(char* patterm, int m, char* text, int n) {
+
+}
+
+vector<int> SM_Algorithm::Knuth_Morris_Pratt(char* patterm, int m, char* text, int n) {
+
+}
