4bb20c5a446de0a92b2028ab0ed7e77b0ac4072f
LnlyPineapple27 <datpt1000cyber@gmail.com>
2019-11-29 14:08:53 +0700

    Function names

--- StringMatchingProj/StringMatchingProj/Board.h
+++ StringMatchingProj/StringMatchingProj/Board.h
@@ -2,5 +2,13 @@
 #include "pch.h"
 class Board
 {
+private:
+	int s_height, s_width;
+
+
+
+public:
+	Board();
+	Board(int )
 };
 
--- StringMatchingProj/StringMatchingProj/SM_Algorithm.h
+++ StringMatchingProj/StringMatchingProj/SM_Algorithm.h
@@ -1,8 +1,21 @@
 #pragma once
 #include "pch.h"
+enum RESULT_STATUS {
+	LEFT_RIGHT,
+	TOP_DOWN,
+	NOT_FOUND
+};
+struct POS {
+	int x, y;
+	RESULT_STATUS status;
+};
+
 class SM_Algorithm
 {
 public:
+	static vector<POS> Brute_Force(char* patterm, int m, char* text, int n);
+	static vector<POS> Kabin_Karp(char* patterm, int m, char* text, int n);
+	static vector<POS> Knuth_Morris_Pratt(char* patterm, int m, char* text, int n);
 
 };
 
