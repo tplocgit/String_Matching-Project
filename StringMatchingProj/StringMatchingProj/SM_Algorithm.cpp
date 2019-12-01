f1a3a489bf3be40129abd35dbef2c3bb24164830
TMThanh7217 <50941195+TMThanh7217@users.noreply.github.com>
2019-12-01 11:47:48 +0700

    Merge KMPTest to master + Add comment for function KMP (#8)

--- StringMatchingProj/StringMatchingProj/SM_Algorithm.cpp
+++ StringMatchingProj/StringMatchingProj/SM_Algorithm.cpp
@@ -54,18 +54,31 @@ vector<int> SM_Algorithm::Kabin_Karp(char* pattern, int m, char* text, int n) {
 	return res;
 }
 
-vector<int> SM_Algorithm::Knuth_Morris_Pratt(char* pattern, char* text) {
-	return {};
+vector<int> SM_Algorithm::Knuth_Morris_Pratt(char* pattern, int pSize, char* text, int tSize) {
+	vector<int> result;
+	vector<int> lps = findLPS(pattern, pSize);
+	int j = 0;// Index of lps for skipping number of characters
+	for (int i = 0; i < tSize; i++) {
+		while (j > 0 && (pattern[j] != text[i]))// Pattern and text doesn't match, reset j
+			j = lps[j - 1];
+		if (pattern[j] == text[i])// If pattern and text match increase j
+			j++;
+		if (j == pSize) {// If j equal pSize that mean the pattern are match
+			result.push_back(i - j + 1);// Save index to result
+			j = lps[j - 1];// Reset j 
+		}
+	}
+	return result;
 }
 
 int SM_Algorithm::String_Hashing(char* patt, int length, int q) {// O(m)
 	if (patt == nullptr)
 		throw "Error: Invalid input";
-	//d: alphabet
-	//m: length of pattern
-	//q: a prime number
-	//h: d^(m - 1) % q
-	//p: result
+	// d: alphabet character
+	// m: length of pattern
+	// q: a prime number
+	// h: d^(m - 1) % q
+	// p: result
 
 	int p = 0;
 
@@ -78,5 +91,21 @@ int SM_Algorithm::String_Hashing(char* patt, int length, int q) {// O(m)
 //------------------------------------------------
 int SM_Algorithm::String_ReHashing(int hash_value, char first_char, char new_char, int h, int q) {// O(1)
 	int t = (d * (hash_value - h * first_char) + new_char) % q;
+	// t may be a negative value, so we need to make it become positive
 	return t >= 0 ? t : t + q;
+}
+
+vector<int> SM_Algorithm::findLPS(char* pattern, int m) {
+	vector<int> lps;
+	lps.resize(m);
+	lps[0] = 0;
+	int j = 0;// Index of lps for saving longest prefix suffix for each position in pattern
+	for(int i = 1; i < m; i++) {
+		while ((pattern[j] != pattern[i]) && j > 0)// Reset j
+			j = lps[j - 1];
+		if (pattern[j] == pattern[i])// Match then increase j
+			j++;
+		lps[i] = j;// Save longest prefix suffix at position i
+	}
+	return lps;
 }
